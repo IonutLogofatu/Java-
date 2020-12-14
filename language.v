a7275ebe3b7ea56f29c02a72b3abbd9eb46395b8
logofatuionut <logofatuionut@yahoo.com>
2020-12-15 00:03:43 +0200

    Update language.v
    Am adaugat noi operatii peste valorile boolene, declarari si doua operatii peste string si functii.

--- language.v
+++ language.v
@@ -3,6 +3,7 @@ Require Import Strings.String.
 Local Open Scope string_scope.
 Local Open Scope list_scope.
 Scheme Equality for string.
+Require Import Coq.Lists.List.
 
 Inductive ErrorNat :=
   | error_nat : ErrorNat
@@ -12,8 +13,17 @@ Inductive ErrorBool :=
   | error_bool : ErrorBool
   | boolean : bool -> ErrorBool.
 
+Inductive errString :=
+  | error_String : errString
+  | newString : string -> errString.
+
+
 Coercion num: nat >-> ErrorNat.
 Coercion boolean: bool >-> ErrorBool.
+
+Inductive StringOP:=
+  | strConcat : errString -> errString -> StringOP
+  | strVar : string -> StringOP.
 
 Inductive AExp :=
   | avar: string -> AExp 
@@ -27,33 +37,73 @@ Inductive AExp :=
   | adec: AExp -> AExp
   | amin: AExp -> AExp -> AExp
   | amax: AExp -> AExp -> AExp
-  | apow: AExp -> AExp.
+  | apow: AExp -> AExp
+  | asqrt: AExp -> AExp
+  | strlenStr: StringOP ->AExp.
+
+
 
 Inductive BExp :=
   | berror
   | btrue
   | bfalse
   | bvar: string -> BExp
+  | bnum : ErrorBool -> BExp
   | blt : AExp -> AExp -> BExp
+  | belt : AExp -> AExp -> BExp
   | bnot : BExp -> BExp
   | band : BExp -> BExp -> BExp
-  | bor : BExp -> BExp -> BExp.
+  | bor : BExp -> BExp -> BExp
+  | bgreather: BExp -> BExp -> BExp
+  | begreather: BExp -> BExp -> BExp
+  | binneq : AExp -> AExp -> BExp
+  | beq : AExp -> AExp -> BExp.
+
 
 Inductive Stmt :=
   | nat_decl: string -> AExp -> Stmt 
   | bool_decl: string -> BExp -> Stmt 
+  | string_decl: string -> StringOP -> Stmt
   | nat_assign : string -> AExp -> Stmt 
   | bool_assign : string -> BExp -> Stmt 
+  | string_assign : string -> StringOP -> Stmt
   | sequence : Stmt -> Stmt -> Stmt
-  | while : BExp -> Stmt -> Stmt
+  | whileStmt : BExp -> Stmt -> Stmt
   | ifthenelse : BExp -> Stmt -> Stmt -> Stmt
-  | ifthen : BExp -> Stmt -> Stmt.
+  | ifthen : BExp -> Stmt -> Stmt
+  | SystemOut : StringOP -> Stmt.
+
+Inductive Result :=
+  | err_undecl : Result
+  | err_assign : Result
+  | default : Result
+  | res_nat : ErrorNat -> Result
+  | res_bool : ErrorBool -> Result
+  | res_string : StringOP -> Result
+  | code : Stmt -> Result. 
+
+
+
+Inductive Glb :=
+    | mainDecl : Stmt -> Glb
+    | functionDecl : string -> list string -> Stmt -> Glb.
+
+
 
 Inductive Mem :=
   | mem_default : Mem
   | offset : nat -> Mem. 
 
 Definition Env := string -> Mem.
+
+Coercion avar : string >-> AExp.
+Coercion bvar : string >-> BExp.
+Coercion strVar : string >-> StringOP.
+Coercion anum: ErrorNat >-> AExp.
+
+
+Notation "String.concat( A , B )" :=(strConcat A B)(at level 49, left associativity).
+Notation "Str.lenght( A )" :=(strlenStr A)(at level 47, left associativity).
 
 Notation "A +' B" := (aplus A B)(at level 50, left associativity).
 Notation "A -' B" := (asub A B)(at level 50, left associativity).
@@ -65,20 +115,48 @@ Notation "-- C" := (adec C)(at level 50, left associativity).
 Notation "min'( A , C )" := (amin A C)(at level 47, left associativity).
 Notation "max'( A , C )" := (amax A C)(at level 47, left associativity).
 Notation "pow'( A )" := (apow A)(at level 47, left associativity).
+Notation "Math.sqrt( A )" :=(asqrt A)(at level 47, left associativity).
 
 
 Notation "A <' B" := (blt A B) (at level 70).
+Notation "A >' B" := (bgreather A B) (at level 70).
 Notation "!' A" := (bnot A)(at level 51, left associativity).
 Notation "A &&' B" := (band A B)(at level 52, left associativity).
 Notation "A ||' B" := (bor A B)(at level 53, left associativity).
+Notation "A <=' B" := (belt A B)(at level 71, left associativity).
+Notation "A >=' B" := (begreather A B)(at level 71, left associativity).
+Notation "A !=' B" := (binneq A B)(at level 72, left associativity).
+Notation "A ==' B" := (beq A B)(at level 72, left associativity).
+
 
 Notation "X :n= A" := (nat_assign X A)(at level 90).
 Notation "X :b= A" := (bool_assign X A)(at level 90).
-Notation "'iNat' X ::= A" := (nat_decl X A)(at level 90).
-Notation "'iBool' X ::= A" := (bool_decl X A)(at level 90).
+Notation "X :s= A" := (string_assign X A)(at level 90).
+Notation "Nat' X ::= A" := (nat_decl X A)(at level 90).
+Notation "boolean' X ::= A" := (bool_decl X A)(at level 90).
+Notation "string' X ::= A" := (string_decl X A)(at level 90).
 Notation "S1 ;; S2" := (sequence S1 S2) (at level 93, right associativity).
-Notation "'fors' ( A ~ B ~ C ) { S }" := (A ;; while B ( S ;; C )) (at level 97).
 
+
+Notation "'fors' ( A ~ B ~ C ) { S }" := (A ;; whileStmt B ( S ;; C )) (at level 97).
+Notation "'If'( B ) 'then' { A }'End'" :=(ifthen B A)(at level 97).
+Notation "'If'( B ) 'then' { S1 }'Else'{ S2 }'End'" := (ifthenelse B S1 S2)(at level 97).
+Notation "'while(' B '){' A '}'" := (whileStmt B A)(at level 95).
+Notation "System.out.println( S )" := (SystemOut S)(at level 98).
+
+
+Notation "'public_void main()'{ S }" := (mainDecl S)(at level 98).
+Notation "'public_void' N (){ S }" := (functionDecl N nil S)(at level 98).
+Notation "'public_void' N ( A ){ S }" := (functionDecl N A S)(at level 98).
+
+Check
+  public_void "test" (){
+     System.out.println( "Asta este un test" )
+  }
+  public_void main(){
+     "testul" :s= "bine ai venit";;
+     System.out.println("str")
+  }.
 
 Reserved Notation "A =[ S ]=> N" (at level 60).
 Reserved Notation "B ={ S }=> B'" (at level 70).
@@ -86,48 +164,24 @@ Reserved Notation "B ={ S }=> B'" (at level 70).
 Reserved Notation "S -{ Sigma }-> Sigma'" (at level 60).
 
 
-Coercion anum: ErrorNat >-> AExp.
-Coercion avar: string >-> AExp.
 
 
-Inductive Result :=
-  | err_undecl : Result
-  | err_assign : Result
-  | default : Result
-  | res_nat : ErrorNat -> Result
-  | res_bool : ErrorBool -> Result
-  | code : Stmt -> Result. 
 
-Scheme Equality for Result.
 
-Definition check_eq_over_types (t1 : Result)(t2 : Result) : bool :=
-  match t1 with
-  | err_assign => match t2 with 
-                   | err_assign => true
-                   | _ => false
-                   end
-  | err_undecl => match t2 with
-                   | err_undecl => true
-                   | _ => false
-                   end
-  | default => match t2 with
-                   | default => true
-                   | _ => false
-                   end
-  | res_nat s => match t2 with
-                   | res_nat s => true
-                   | _ => false
-                   end
-  | res_bool s => match t2 with 
-                   | res_bool s => true
-                   | _ => false
-                   end
-  | code s => match t2 with
-                   | code s => true
-                   | _ => false
-                   end
-  end.
 
-Compute (check_eq_over_types (res_nat 1000) (res_nat 100)). 
-Compute (check_eq_over_types err_undecl (res_nat 100)). 
-Compute (check_eq_over_types err_undecl (code ("x" :n= 4))).
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
